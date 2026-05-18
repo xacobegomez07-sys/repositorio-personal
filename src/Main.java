b250d5cc202e01efe574089bfbfd84637818737b
jgoomezzz <xacobegomez07@gmail.com>
2026-05-18 13:54:48 +0200

    Inicio de proyecto

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("ESTO ES AL MAIN CON MODIFICACIONES");
+
+        /*Comentario de Jacobo fumón*/
     }
 }
